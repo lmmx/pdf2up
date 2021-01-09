a775f70e6d834d14cce09aaf8be88e2bff1de045
Louis Maddox <louismmx@gmail.com>
2021-01-09 15:56:05 +0000

    Initial working utility; added progress bar and early stop if unpaired page

--- crop_and_2_up.py
+++ crop_and_2_up.py
@@ -1,7 +1,12 @@
 from pathlib import Path
+from tqdm import tqdm
+from sys import stderr
 from imageio import imread, imwrite
 from argparse import ArgumentParser
 from subprocess import call
+from pdf2image import convert_from_path
+from more_itertools import ichunked
+from PIL import Image
 
 parser = ArgumentParser()
 parser.add_argument("input")
@@ -25,3 +30,23 @@ call([
     str(crop_pdf_dest)
     ]
 )
+
+page_limit = 8
+pdf_pages = convert_from_path(crop_pdf_dest)
+i = 0
+pdf_pages_lim = pdf_pages[:page_limit]
+for page_pair in tqdm(ichunked(pdf_pages_lim, 2), total=len(pdf_pages_lim) // 2):
+    iter_size = len(pdf_pages_lim[(i * 2) : (i + 1) * 2])
+    if iter_size == 1:
+        print(f"Stopped ahead of iteration {i+1} to avoid unpaired page", file=stderr)
+        continue
+    p1, p2 = page_pair
+    if not p1.height == p2.height:
+        raise NotImplementedError("Images aren't same size, can't stack 2-up")
+    combined_shape = (p1.width + p2.width, p1.height)
+    two_up = Image.new('RGB', combined_shape)
+    two_up.paste(p1, (0,0))
+    two_up.paste(p2, (p1.width, 0))
+    out_png = input_pdf.parent / f"{input_pdf.stem}_{i}.png"
+    two_up.save(out_png)
+    i += 1
