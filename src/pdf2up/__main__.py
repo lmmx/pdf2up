ea59fa45dbec5ab09aef3914d845101eb6f6d331
Louis Maddox <louismmx@gmail.com>
2022-06-07 18:48:16 +0100

    CLI fix

--- src/pdf2up/__main__.py
+++ src/pdf2up/__main__.py
@@ -9,7 +9,17 @@ from .conversion import pdf2png
 def main() -> list[Path]:
     parser = Pdf2upParser()
     arg_l = parser.parse_args()
-    pdf2png(**vars(arg_l))
+    config_dict = vars(arg_l)
+    if config_dict["box"] is None:
+        config_dict["box"] = [0,0,0,0]
+    config_dict = {
+        k: v for k, v in vars(arg_l).items()
+        if (
+            v is not None
+            or k in ["skip"] # skip can be passed as None and is required
+        )
+    }
+    pdf2png(**config_dict)
     return  # Don't return the new PNG Paths when called on command line
 
 
